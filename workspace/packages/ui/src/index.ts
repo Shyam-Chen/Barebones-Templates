1babe0c546f1ecdf345d1b52fb92f7714556f3f0
Shyam-Chen <shyamchen1994@gmail.com>
2023-08-18 14:23:11 +0800

    13th Commit

--- workspace/packages/ui/src/index.ts
+++ workspace/packages/ui/src/index.ts
@@ -1,1 +1,2 @@
 export { default as XButton } from './components/XButton.vue';
+export { default as XSwitch } from './components/XSwitch.vue';
--- workspace/packages/web/src/App.vue
+++ workspace/packages/web/src/App.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import { RouterView } from 'vue-router';
-import { XButton } from '@x/ui';
+import { XButton, XSwitch } from '@x/ui';
+
+const status = ref(false);
 </script>
 
 <template>
   <RouterView />
   <XButton color="info">X UI</XButton>
+  <XSwitch v-model:value="status" />
 </template>
