d7340702998c7961f0e35a97a37448a94d087ab9
Shyam-Chen <shyamchen1994@gmail.com>
2023-08-21 10:28:33 +0800

    15th Commit

---
+++ workspace/packages/auth/src/routes/(sign-in)/+page.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+// import TextField from '~/components/TextField.vue';
+// import Button from '~/components/Button.vue';
+// import Spinner from '~/components/Spinner.vue';
+// import Link from '~/components/Link.vue';
+
+// import useStore from './store';
+// import useLocale from './locales';
+// import useSchema from './schema';
+
+// const { state, actions, $reset } = useStore();
+// const locale = useLocale();
+// const schema = useSchema();
+
+const sub = ref<HTMLIFrameElement>();
+
+function signIn() {
+  // if (schema.validate()) {
+  //   actions.signIn();
+  // }
+
+  const data = { accessToken: 'foobar' };
+
+  sub.value?.contentWindow?.postMessage(JSON.stringify(data), 'http://127.0.0.1:5174');
+
+  location.assign('http://127.0.0.1:5174');
+}
+
+// onUnmounted(() => {
+//   $reset();
+// });
+</script>
+
+<template>
+  <iframe ref="sub" src="http://127.0.0.1:5174" class="hidden"></iframe>
+
+  <div class="w-full max-w-sm">
+    <form class="bg-white dark:bg-slate-800 shadow-md rounded px-8 pt-6 pb-8">
+      <div class="mb-8">
+        <div class="text-slate-900 dark:text-white font-bold text-xl mb-2">
+          Sign in to our platform
+        </div>
+
+        <div>Login here using your username and password</div>
+      </div>
+
+      <div class="mb-4">
+        Username
+        <input class="border" />
+      </div>
+
+      <div class="mb-8">
+        Password
+        <input type="password" class="border" />
+      </div>
+
+      <button type="button" class="border px-2 py-1" @click="signIn">Sign in</button>
+    </form>
+  </div>
+</template>
--- workspace/packages/web/src/App.vue
+++ workspace/packages/web/src/App.vue
@@ -4,6 +4,18 @@ import { RouterView } from 'vue-router';
 import { XButton, XSwitch } from '@x/ui';
 
 const status = ref(false);
+
+window.addEventListener('message', (event) => {
+  // Check the origin of the message to ensure security
+  if (event.origin === 'http://localhost:5173') {
+    // Parse the data and store it in LocalStorage
+    const receivedData = JSON.parse(event.data);
+    localStorage.setItem('accessToken', JSON.stringify(receivedData));
+
+    // Do something with the synced data
+    console.log('Received data:', receivedData);
+  }
+});
 </script>
 
 <template>
