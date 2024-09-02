b48f9fd4920c1497a890cbc1c24849aae5bc9a2b
Shyam-Chen <shyamchen1994@gmail.com>
2024-09-02 15:05:00 +0800

    54th Commit

--- tauri/vite.config.ts
+++ tauri/vite.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '~': resolve(__dirname, 'src'),
+      '~': resolve(import.meta.dirname, 'src'),
     },
   },
   clearScreen: false,
